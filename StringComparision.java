9f925523f294415ae8655b7be30d4311adddfab1
Riya-Soni <36445899+Riya-Soni@users.noreply.github.com>
2019-02-12 13:33:06 +0530

    Update StringComparision.java
    Sequence of string is given as per the comment

--- StringComparision.java
+++ StringComparision.java
@@ -1,9 +1,15 @@
 public class StringComparision {
     public static void main(String[] args){
-        String s1= "Riyu";
-        String s2 = "Riya";
+        String s1= "GitHub";
+        String s2 = "GitHua";
         
         int i = s1.compareTo(s2);
         System.out.println(i);
         }
 }
+/*
+this will check the string as per dictionary sequence by default,
+that means no  restriction for string length
+but this is case sensitive so, it will arrange Capital letters first then according their UNICODE value
+if number came, they before any letter
+*/
