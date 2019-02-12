32490fbe8e9632dcdb7dbd0b53dcb1e066b6211e
Riya-Soni <36445899+Riya-Soni@users.noreply.github.com>
2019-02-12 13:12:57 +0530

    Add files via upload

---
+++ StringComparision.java
@@ -0,0 +1,9 @@
+public class StringComparision {
+    public static void main(String[] args){
+        String s1= "Riyu";
+        String s2 = "Riya";
+        
+        int i = s1.compareTo(s2);
+        System.out.println(i);
+        }
+}
