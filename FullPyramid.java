6aa70463ae54ff7d4cc6f0f43eede4d67383eebb
Riya-Soni <36445899+Riya-Soni@users.noreply.github.com>
2019-02-23 12:02:15 +0530

    Update FullPyramid.java

--- FullPyramid.java
+++ FullPyramid.java
@@ -1,7 +1,4 @@
-
-import java.util.Scanner;
-
-/*
+/*Output will be
               * 
             * * * 
           * * * * * 
@@ -10,21 +7,19 @@ import java.util.Scanner;
     * * * * * * * * * * * 
   * * * * * * * * * * * * * 
 * * * * * * * * * * * * * * * 
-
 */
+import java.util.Scanner;
 public class FullPyramid {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of rows: ");
         int row = sc.nextInt();
-        for(int i=0; i<=row; i++){
-            for(int j=0; j < (row-i); j++)
+        for(int i=1; i<=row; i++){
+            for(int j=1; j <= (row-i); j++)
                     System.out.print("  ");
             for(int k = 1; k<=(2*i)-1; k++)
                     System.out.print("* ");
             System.out.println("");
         }
-        
-        
     }
 }
