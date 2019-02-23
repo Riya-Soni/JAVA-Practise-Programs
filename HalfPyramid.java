14c79405d1b87ea749dd841f4d4ef2d516a67955
Riya-Soni <36445899+Riya-Soni@users.noreply.github.com>
2019-02-23 12:03:44 +0530

    Update HalfPyramid.java

--- HalfPyramid.java
+++ HalfPyramid.java
@@ -1,7 +1,4 @@
-
-import java.util.Scanner;
-
-/*  8 rows
+/*Output for 8 rows:
 * 
 * * 
 * * * 
@@ -10,7 +7,9 @@ import java.util.Scanner;
 * * * * * * 
 * * * * * * * 
 * * * * * * * * 
- */
+*/
+
+import java.util.Scanner;
 public class HalfPyramid {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
